52ad90aadd0390bd21b62cc74113d5db2a29621c
Zprojects <jelinares04@gmail.com>
2026-06-24 23:29:24 -0600

    Add GPL v3 copyright headers to source files

--- source/core/translator.py
+++ source/core/translator.py
@@ -1,3 +1,19 @@
+# OCR Translator
+# Copyright (C) 2026 ZProjects
+#
+# This file is part of OCR Translator.
+# OCR Translator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OCR Translator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OCR Translator. If not, see <https://www.gnu.org/licenses/>.
 from __future__ import annotations
 
 import re
